eabc3d5cb1ca4857094a79aec23b07163718495d
Adam Sulewski <adam.j.sulewski@gmail.com>
2019-11-16 10:14:16 -0500

    alias client

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Spotify.Mixfile do
   def project do
     [
       app: :spotify_ex,
-      version: "2.0.10",
+      version: "2.0.11",
       elixir: ">= 1.9.2",
       description: description(),
       package: package(),
